c35e4d73bafa8f5b3b8d88e4d861a856aa74c3ce
Afrânio Honório de Oliveira <afranio@outlook.com>
2014-04-08 22:55:00 -0300

    Projeto primeira entrega baseline1

--- produto/codigo/Carofour/Controllers/CarrinhoController.cs
+++ produto/codigo/Carofour/Controllers/CarrinhoController.cs
@@ -46,8 +46,23 @@ namespace Carofour.Controllers
                 TempData["Mensagem"] = "O Cliente foi cadastrado com sucesso !";
             }
             return View("FecharCompra", cliente);
-
         }
 
+        public ActionResult VerCarrinho(Produto produto)
+        {
+            var produtos = new List<Produto>
+            { 
+                new Produto { id=2, nome = "Farinha de Trigo", descricao = "Farinha de Trigo bonita", preco = 2.20, urlImagem = "/Content/Images/farinhaDeTrigo.jpg"},
+                new Produto { id=2, nome = "Carne Moída", descricao = "Carne Moída bonita", preco = 12.30, urlImagem = "/Content/Images/carneMoida.jpg"}
+            };
+            return View(produtos);
+        }
+
+        public ActionResult InformacoesDaCompra()
+        {
+            //List<Categoria> categoriasMapeadas = this.MapearCategorias();
+            //Categoria categoriaRetorno = categoriasMapeadas.Where(c => c.nome == categoria).FirstOrDefault() as Categoria;
+            return View();
+        }
     }
 }
